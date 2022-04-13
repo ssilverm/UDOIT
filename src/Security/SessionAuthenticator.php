edf41541d0ea8491d7bf642c7f654472e0312e00
Robert Boyd III <ro962471@ucf.edu>
2022-04-13 09:53:23 -0400

    Try upgrade

--- src/Security/SessionAuthenticator.php
+++ src/Security/SessionAuthenticator.php
@@ -9,14 +9,16 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
-class SessionAuthenticator extends AbstractGuardAuthenticator
+class SessionAuthenticator extends AbstractAuthenticator
 {
     private $em;
     private $sessionService;
@@ -35,12 +37,18 @@ class SessionAuthenticator extends AbstractGuardAuthenticator
         $this->logger = $logger;
     }
 
-    public function supports(Request $request)
+    public function supports(Request $request): bool
     {
         $this->logger->error("supports()");
         return $this->sessionService->hasSession();
     }
 
+    public function authenticate(Request $request): Passport
+    {
+        return new SelfValidatingPassport(new UserBadge($this->sessionService->getSession()->get('userId')));
+    }
+
+    /*
     public function getCredentials(Request $request)
     {
         $this->logger->error("getCredentials()");
@@ -55,25 +63,28 @@ class SessionAuthenticator extends AbstractGuardAuthenticator
         $this->logger->error("creds are: {$credentials}");
         return is_numeric($credentials);
     }
+    
 
     public function getUser($userId, UserProviderInterface $userProvider)
     {
         $this->logger->error("getUser()");
         return $this->em->getRepository(User::class)->find($userId);
     }
+     */
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): ?JsonResponse
     {
         $this->logger->error("onAuthenticationSuccess()");
         return null;
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception) 
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?JsonResponse
     {
         $this->logger->error("onAuthenticationFailure()");
         return null;
     }
 
+    /*
     public function start(Request $request, AuthenticationException $exception = null) 
     {
         $this->logger->error("start()");
@@ -89,4 +100,5 @@ class SessionAuthenticator extends AbstractGuardAuthenticator
     {
         $this->logger->error("supportsRememberMe()");
     }
+     */
 }
